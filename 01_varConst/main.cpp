2eaf018a11370d606180f0c7c38ba8a73d20bcd4
drmzgithub <afraz.drmz@gmail.com>
2025-06-09 23:24:43 +0530

    new begining

---
+++ 01_varConst/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // mutable variable means that value will be change or re initialise will be allowed 
+    int num = 52343;
+    cout << "immutable " << num << endl;
+    // immutable variable means that value will be not change or re initialise will be not allowed
+    const int num2 = 643653;
+    cout << "immutable " << num2 << endl;
+
+    cout << "welcome to chai or code me \n";
+    cout << "hello world";
+};
